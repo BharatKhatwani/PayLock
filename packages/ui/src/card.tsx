378a0ecf11776858e0a69f83ee3f311807b460de
Bharat Khatwani <bharatkhatwani06@gmail.com>
2025-09-06 10:38:30 +0000

    'update the ui components'

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -1,14 +1,16 @@
 export function Card({
   title,
   children,
+  className = "",   // ✅ add default
 }: {
   title: string;
   children?: React.ReactNode;
+  className?: string;   
 }): JSX.Element {
   return (
-    <div className="border p-6 bg-white rounded-xl  ">
+    <div className={`border p-6 bg-white rounded-xl ${className}`}>
       <h1 className="text-xl border-b pb-2">{title}</h1>
-      <div >{children}</div>
+      <div>{children}</div>
     </div>
   );
 }
